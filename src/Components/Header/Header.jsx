be9794bba213652bd31e5cf7fa96dab98197691a
hridoy-ahmmad <hridoyp338y@gmail.com>
2026-04-13 23:32:23 +0600

    first commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,45 @@
+"use client"
+import { ChartLine, Clock4, House } from 'lucide-react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React from 'react';
+
+const Header = () => {
+    const pathName = usePathname()
+    console.log(pathName);
+
+
+    const nav = <>
+        <li><Link href={'/'} > <House /> Home</Link></li>
+        <li><Link href={'/time_line'}> <Clock4 /> Time Line</Link></li>
+        <li><Link href={'/stats'}> <ChartLine /> Stats</Link></li>
+    </>
+
+
+    return (
+        <div className='container mx-auto'>
+            <div className="navbar ">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </div>
+                        <ul
+                            tabIndex="-1"
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow text-[#64748B]">
+                            {nav}
+                        </ul>
+                    </div>
+                    <a className="btn btn-ghost text-xl">Keen Keeper</a>
+                </div>
+                <div className="navbar-end hidden lg:flex text-[#64748B]" >
+                    <ul className="menu menu-horizontal px-1">
+                        {nav}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/app/time_line/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TimeLine = () => {
+    return (
+        <div>
+            timeline page
+        </div>
+    );
+};
+
+export default TimeLine;
